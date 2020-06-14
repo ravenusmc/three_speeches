d3ee94c4c0271440e041debc966abe2a35dfd34a
ravenusmc <mcuddy77@gmail.com>
2020-06-14 17:17:02 -0400

    setting up study page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-
-    <!-- <router-view/>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
+    
     <Navbar />
 
     <!-- Header area -->
