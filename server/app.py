1c6177186ec20dd88c972e424ca186c8c29d5b3b
ravenusmc <mcuddy77@gmail.com>
2020-06-26 16:34:29 -0400

    working on new route for sentiment

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 
 #importing code that I wrote
 from study import *
+from sentiment import *
 
 # configuration
 DEBUG = True
@@ -16,6 +17,16 @@ CORS(app)
 
 #This route will get the data for the charts
 @app.route('/getWordCountData', methods=['GET', 'POST'])
+def routeOne():
+    if request.method == 'POST':
+        study = Words()
+        post_data = request.get_json()
+        selected_speech = post_data['speech']
+        chartData = study.build_word_chart(selected_speech)
+        return jsonify(chartData)
+
+#This route will get the sentiment for the charts
+@app.route('/getWordCountData', methods=['GET'])
 def routeOne():
     if request.method == 'POST':
         study = Words()
